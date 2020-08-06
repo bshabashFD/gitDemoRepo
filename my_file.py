ec9770ec41d76842c302bd0acd0ee6dc4cb1b318
Boris Shabash <50187583+bshabashFD@users.noreply.github.com>
2020-08-06 14:29:23 -0700

    Changes made by "Doug"

--- my_file.py
+++ my_file.py
@@ -2,8 +2,9 @@ import numpy as np
 import pandas as pd
 
 
-matrix_A = np.random.randn(2, 3)
-matrix_B = np.random.randn(3, 8)
+matrix_A = np.random.randn(2, 8)
+matrix_B = np.random.randn(8, 8)
+matrix_C = np.random.randn(4, 4)
 
 
 final_result = np.matmul(matrix_A, matrix_B)
