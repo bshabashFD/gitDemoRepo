a1659bf9280c6522489d27bf0f1a364d84a2123a
bshabashFD <bshabash007@outlook.com>
2020-08-06 14:13:53 -0700

    changed the dimensions of Matrix A and B

--- my_file.py
+++ my_file.py
@@ -2,9 +2,12 @@ import numpy as np
 import pandas as pd
 
 
-matrix_A = np.random.randn(2,3)
-matrix_B = np.random.randn(3, 4)
+matrix_A = np.random.randn(2, 5)
+matrix_B = np.random.randn(5, 6)
 
 
 final_result = np.matmul(matrix_A, matrix_B)
+
+assert(final_result.shape[0] == matrix_A.shape[0])
 print(final_result.shape)
+
