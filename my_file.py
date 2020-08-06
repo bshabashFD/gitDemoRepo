4a2164fedab585533d104000db5efccd253a767f
bshabashFD <bshabash007@outlook.com>
2020-08-06 14:30:41 -0700

    changed the matrices again

--- my_file.py
+++ my_file.py
@@ -2,8 +2,9 @@ import numpy as np
 import pandas as pd
 
 
-matrix_A = np.random.randn(2, 3)
-matrix_B = np.random.randn(3, 8)
+matrix_A = np.random.randn(5, 5)
+matrix_B = np.random.randn(5, 8)
+matrix_Z = np.random.randn(3, 3)
 
 
 final_result = np.matmul(matrix_A, matrix_B)
