f0ec81f66651fc1f1022fba452388c495acfb9de
bshabashFD <bshabash007@outlook.com>
2020-08-06 14:27:39 -0700

    changed some of the matrix sizes again

--- my_file.py
+++ my_file.py
@@ -2,8 +2,8 @@ import numpy as np
 import pandas as pd
 
 
-matrix_A = np.random.randn(2, 4)
-matrix_C = np.random.randn(4, 8)
+matrix_A = np.random.randn(2, 3)
+matrix_B = np.random.randn(3, 8)
 
 
 final_result = np.matmul(matrix_A, matrix_B)
