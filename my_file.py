24f7b9570cae2d03c96c39368a64aacb513c118e
bshabashFD <bshabash007@outlook.com>
2020-08-06 14:46:08 -0700

    Reverted to previous version f0ec81, and then added some text to print

--- my_file.py
+++ my_file.py
@@ -2,14 +2,12 @@ import numpy as np
 import pandas as pd
 
 
-
-matrix_A = np.random.randn(2, 8)
-matrix_B = np.random.randn(8, 8)
-matrix_Z = np.random.randn(3, 3)
+matrix_A = np.random.randn(2, 3)
+matrix_B = np.random.randn(3, 8)
 
 
 final_result = np.matmul(matrix_A, matrix_B)
 
 assert(final_result.shape[0] == matrix_A.shape[0])
-print(final_result.shape)
+print("The shape of the final result is ",final_result.shape)
 
